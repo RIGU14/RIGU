210fb8b7c8347b7794978d15bc3f8f6df2783fb4
RIGU <cartaura7@gmail.com>
2026-08-01 10:41:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-// CartAura JavaScript
-console.log("CartAura Loaded Successfully!");
+console.log("RIGU Loaded");
 
-alert("Welcome to RIGU!");
+fetch("products.json")
+  .then(response => response.json())
+  .then(products => {
+
+    const grid = document.querySelector(".product-grid");
+    grid.innerHTML = "";
+
+    products.forEach(product => {
+
+      if (!product.title) return;
+
+      grid.innerHTML += `
+        <div class="product-card">
+          <img src="${product.image}" alt="${product.title}">
+          <h3>${product.title}</h3>
+          <p class="price">₹${product.price}</p>
+
+          <div class="buttons">
+            <button class="cart-btn">🛒 Add to Cart</button>
+            <button class="buy-btn">⚡ Buy Now</button>
+          </div>
+        </div>
+      `;
+    });
+
+    console.log(products.length + " Products Loaded");
+
+  })
+  .catch(error => console.log(error));
