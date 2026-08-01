9e703bc8f560395817885a837b5ef2a2d38f987c
RIGU <cartaura7@gmail.com>
2026-08-01 12:14:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-console.log("RIGU Working");
-alert("Website Working");
+fetch("products.json")
+.then(res => res.json())
+.then(products => {
+
+const productGrid = document.querySelector(".product-grid");
+
+productGrid.innerHTML = "";
+
+products.forEach(product => {
+
+productGrid.innerHTML += `
+<div class="product-card">
+
+<img src="${product.image}" alt="${product.title}">
+
+<h3>${product.title}</h3>
+
+<p class="price">₹${product.price}</p>
+
+<div class="buttons">
+<button class="cart-btn">🛒 Add to Cart</button>
+<button class="buy-btn">⚡ Buy Now</button>
+</div>
+
+</div>
+`;
+
+});
+
+})
+.catch(err => {
+console.log(err);
+alert("Products Load Error");
+});
