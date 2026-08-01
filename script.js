0abe7847f7d6fa6ca9fff697e3a68c608a5c04ac
RIGU <cartaura7@gmail.com>
2026-08-01 12:44:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-console.log("RIGU Version 2 Loaded");
-
-const products = [
-  {
-    title: "Sports Shoes",
-    price: 1499,
-    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500"
-  },
-  {
-    title: "Smart Watch",
-    price: 2999,
-    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500"
-  }
-];
+fetch("products.json")
+.then(res => res.json())
+.then(products => {
 
 const productGrid = document.querySelector(".product-grid");
 
 productGrid.innerHTML = "";
 
-products.forEach(product => {
+products.slice(0,2).forEach(product => {
 
-  productGrid.innerHTML += `
-    <div class="product-card">
+productGrid.innerHTML += `
+<div class="product-card">
 
-      <img src="${product.image}" alt="${product.title}">
+<img src="${product.image}" alt="${product.title}">
 
-      <h3>${product.title}</h3>
+<h3>${product.title}</h3>
 
-      <p class="price">₹${product.price}</p>
+<p class="price">₹${product.price}</p>
 
-      <div class="buttons">
-        <button class="cart-btn">🛒 Add to Cart</button>
-        <button class="buy-btn">⚡ Buy Now</button>
-      </div>
+<div class="buttons">
+<button class="cart-btn">🛒 Add to Cart</button>
+<button class="buy-btn">⚡ Buy Now</button>
+</div>
 
-    </div>
-  `;
+</div>
+`;
 
 });
+
+})
+.catch(err => console.log(err));
