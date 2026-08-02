cae9dfa67ae3a5abf6f95832e8c45bf76c065216
RIGU <cartaura7@gmail.com>
2026-08-02 16:28:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyCqw2NlWRRXftz5p9FSNh3GT7Z0tnLlKIc",
+  apiKey: "AIzaSyCqw2NlWRRXftz5p9FSNh3GT7Z0tnLlKIc",
   authDomain: "rigu-shop.firebaseapp.com",
   projectId: "rigu-shop",
   storageBucket: "rigu-shop.firebasestorage.app",
   messagingSenderId: "38228750370",
-  appId: "1:38228750370:web:3e45565af69af32f015a7b"
+  appId: "1:38228750370:web:3e45565af69af32f015a7b",
+  measurementId: "G-4STGZZW7W9"
 };
 
 const app = initializeApp(firebaseConfig);
