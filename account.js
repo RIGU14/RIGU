ebaf344ef95fcead40fb5cdbc05e0e3d5767a60a
RIGU <cartaura7@gmail.com>
2026-08-02 16:16:36 +0530

    Create account.js

---
+++ account.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "ਤੁਹਾਡੀ apiKey",
+  authDomain: "rigu-shop.firebaseapp.com",
+  projectId: "rigu-shop",
+  storageBucket: "rigu-shop.firebasestorage.app",
+  messagingSenderId: "38228750370",
+  appId: "1:38228750370:web:3e45565af69af32f015a7b",
+  measurementId: "G-4STGZZW7W9"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const accountBtn = document.getElementById("accountBtn");
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    accountBtn.innerHTML = `<i class="fa-solid fa-user"></i> ${user.email}`;
+    accountBtn.href = "#";
+
+    accountBtn.onclick = () => {
+      if (confirm("Logout?")) {
+        signOut(auth).then(() => {
+          location.reload();
+        });
+      }
+    };
+  }
+});
