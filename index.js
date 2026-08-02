eca4d41fa2929b5a7143a352f20ad2684de4748e
RIGU <cartaura7@gmail.com>
2026-08-02 16:53:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,45 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import {
   getAuth,
-  onAuthStateChanged,
-  signOut
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCqw2NlWRRXftz5p9FSNh3GT7Z0tnLlKIc",
   authDomain: "rigu-shop.firebaseapp.com",
   projectId: "rigu-shop",
   storageBucket: "rigu-shop.firebasestorage.app",
   messagingSenderId: "38228750370",
-  appId: "1:38228750370:web:3e45565af69af32f015a7b"
+  appId: "1:38228750370:web:3e45565af69af32f015a7b",
+  measurementId: "G-4STGZZW7W9"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const accountBtn = document.getElementById("accountBtn");
-const dropdown = document.getElementById("accountDropdown");
-const userAvatar = document.getElementById("userAvatar");
-const avatarLarge = document.getElementById("avatarLarge");
-const userName = document.getElementById("userName");
-const userEmail = document.getElementById("userEmail");
-const logoutBtn = document.getElementById("logoutBtn");
 
 onAuthStateChanged(auth, (user) => {
 
   if (user) {
 
-    const firstLetter = user.email.charAt(0).toUpperCase();
+    const firstLetter = (user.displayName || user.email)
+      .charAt(0)
+      .toUpperCase();
 
-    userAvatar.textContent = firstLetter;
-    avatarLarge.textContent = firstLetter;
+    accountBtn.innerHTML = `
+      <span id="userAvatar">${firstLetter}</span>
+    `;
 
-    userName.textContent = user.displayName || "RIGU User";
-    userEmail.textContent = user.email;
+    accountBtn.href = "account.html";
 
-    accountBtn.href = "#";
+  } else {
 
-    accountBtn.onclick = (e) => {
-      e.preventDefault();
-      dropdown.classList.toggle("active");
-    };
+    accountBtn.innerHTML = `
+      <i class="fa-regular fa-user"></i> Login
+    `;
 
-    logoutBtn.onclick = (e) => {
-      e.preventDefault();
-
-      if (confirm("Logout?")) {
-        signOut(auth).then(() => {
-          location.reload();
-        });
-      }
-    };
-
-    document.addEventListener("click", (e) => {
-      if (!document.querySelector(".account-menu").contains(e.target)) {
-        dropdown.classList.remove("active");
-      }
-    });
-
+    accountBtn.href = "login.html";
   }
 
 });
